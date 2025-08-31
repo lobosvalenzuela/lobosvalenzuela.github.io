a2631402aa7b9efff1106308bbaabb9ccd45709a
lobosvalenzuela <t.lobos@duocuc.cl>
2025-08-30 22:29:12 -0400

    function addTask

--- scripts/main.js
+++ scripts/main.js
@@ -27,9 +27,36 @@ function dataForm(ageForm){
     console.log(JSON.stringify(Object.fromEntries(formData), null, 2));
 }
 
-function addTask(task){
-    
-}
+document.addEventListener('DOMContentLoaded', function(){
+    // Get the dom elements that we r going to use 
+    const newTask = document.getElementById("newTask");
+    const addButton = document.getElementById("addButton");
+    const taskList = document.getElementById("taskList");
+    // Stores tasks
+    let tasks = [];
+    let taskId = 0;
+    // Add task to the list
+    function addTask(){
+        const taskText = newTask.value.trim();
+
+        if (taskText === ''){
+            alert("please enter a task")
+            return;
+        }
+        const task = {
+            id : taskId++,
+            text : taskText,
+            completed : false
+        }
+        tasks.push(task);
+        newTask.value = '';
+
+        renderTasks();
+    }
+
+
+
+});
 //age form
 document.getElementById("ageForm").addEventListener("submit", function(e){
     console.log(e);
