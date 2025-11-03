db8d5a404daab8c6f98a86b19650560a4a34d47f
lobosvalenzuela <t.lobos@duocuc.cl>
2025-11-02 22:17:29 -0300

    jj

--- elitaliano/app/layout.tsx
+++ elitaliano/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";                  // aquí ya importas bootstrap.css (como acordamos)
-import BootstrapClient from "./bootstrap-client";
+import "./globals.css"; 
+import BootstrapClient from "./bootstrap-client";                 // aquí ya importas bootstrap.css (como acordamos)
 import Navbar from "./navbar";
 import "./globals.css";
-
-
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,11 +25,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="es">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* Habilita JS de Bootstrap */}
-        <BootstrapClient />
+        
         {/* Navbar fija para todas las páginas */}
         <Navbar />
         {/* Contenido de cada página */}
         {children}
+        <BootstrapClient />
       </body>
     </html>
   );
