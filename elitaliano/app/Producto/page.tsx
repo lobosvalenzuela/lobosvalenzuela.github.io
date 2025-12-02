e416a3cff8f38ebd72d62904a86279e6f1b0c643
lobosvalenzuela <t.lobos@duocuc.cl>
2025-12-01 23:49:06 -0300

    arreglo page producto

--- elitaliano/app/Producto/page.tsx
+++ elitaliano/app/Producto/page.tsx
@@ -1,8 +1,5 @@
-// app/Producto/page.tsx
 import React from "react";
 import Producto from "./producto";
-import { products } from "./products";
-
 export default function Page() {
-  return <Producto products={products} />;
+  return <Producto />;
 }
